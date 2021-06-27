54246c3efaa6b26d8f20b9db0ef356a23386343e
Pei Hsuan <joanneh1109@gmail.com>
2021-06-27 15:38:43 +0800

    drink test6(加料2&迴圈1)

--- src/main/java/com/fju/sparklingdrink/Tester.java
+++ src/main/java/com/fju/sparklingdrink/Tester.java
@@ -8,46 +8,57 @@ public class Tester {
         Ice[] ices = {new Ice1(), new Ice2(), new Ice3(), new Ice4()};
         Topping[] toppings = {new Topping1(), new Topping2(), new Topping3()};
 
-        System.out.println("[氣泡水菜單]");
-        for (Drink drink : drinks) {
-            System.out.print(drink.no);
-            System.out.print(drink.getName());
-            System.out.println(" $" + drink.getPrice());
-        }
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("請選擇口味：");
-        int kind = Integer.parseInt(scanner.next());
-        System.out.println("你選擇的口味是： " + drinks[kind-1].getName());
-        //System.out.println("金額：" + drinks[kind-1].getPrice());
+        int total;
+        int end = -1;
 
-
-        System.out.println("[選擇冰量]");
-        for (Ice ice : ices) {
-            System.out.print(ice.ino + " ");
-            System.out.println(ice.getName());
-        }
-        System.out.println("請選擇冰量：");
-        kind = Integer.parseInt(scanner.next());
-        System.out.println("你選擇的冰量是： " + ices[kind-1].getName());
-
-        System.out.println("[加料區]");
-        System.out.println("要加料嗎？Y/N");
-        String y = "Y";
-        String topp = scanner.next();
-        if (topp.equals("Y")) {
-            System.out.println("[選擇配料]");
-            for (Topping topping : toppings) {
-                System.out.print(topping.tno);
-                System.out.print(topping.getName());
-                System.out.println(" $" + topping.getPrice());
+        while (end != 0){
+            System.out.println("[氣泡水菜單]");
+            for (Drink drink : drinks) {
+                System.out.print(drink.no);
+                System.out.print(drink.getName());
+                System.out.println(" $" + drink.getPrice());
             }
-            System.out.println("請選擇配料：");
-            kind = Integer.parseInt(scanner.next());
-            System.out.print("你選擇的配料是：" + toppings[kind-1].getName());
-            //System.out.println(" $" + toppings[kind-1].getPrice());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("請選擇口味：");
+            int kind = Integer.parseInt(scanner.next());
+            System.out.println("你選擇的口味是： " + drinks[kind-1].getName());
+            //System.out.println("金額：" + drinks[kind-1].getPrice());
 
+
+            System.out.println("[選擇冰量]");
+            for (Ice ice : ices) {
+                System.out.print(ice.ino + " ");
+                System.out.println(ice.getName());
+            }
+            System.out.println("請選擇冰量：");
+            kind = Integer.parseInt(scanner.next());
+            System.out.println("你選擇的冰量是： " + ices[kind-1].getName());
+
+            System.out.println("[加料區]");
+            System.out.println("要加料嗎？Y/N");
+            String topp = scanner.next();
+            if (topp.equals("Y")) {
+                System.out.println("[選擇配料]");
+                for (Topping topping : toppings) {
+                    System.out.print(topping.tno);
+                    System.out.print(topping.getName());
+                    System.out.println(" $" + topping.getPrice());
+                }
+                System.out.println("請選擇配料：");
+                kind = Integer.parseInt(scanner.next());
+                System.out.println("你選擇的配料是：" + toppings[kind-1].getName());
+                //System.out.println(" $" + toppings[kind-1].getPrice());
+                total = drinks[kind-1].getPrice()+toppings[kind-1].getPrice();
+
+                System.out.println("總金額：" + total);
+            }else if (topp.equals("N")) {
+                total = drinks[kind-1].getPrice();
+                System.out.println("總金額："+total);
+            }
 
         }
+
+
 
 
     }
